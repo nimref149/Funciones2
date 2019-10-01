d99827ef0febc2ebcd869bedb762bd5fffbc0d98
nimref149 <cristian-149@hotmail.com>
2019-10-01 06:35:10 -0300

    nuevo

--- funciones/funciones.h
+++ funciones/funciones.h
@@ -1,96 +1,18 @@
-int getInt2(char message[],int lowLimit,int hiLimit);
-/**
- * \brief Solicita un número al usuario y lo valida
- * \param input Se carga el numero ingresado
- * \param message Es el mensaje a ser mostrado
- * \param eMessage Es el mensaje a ser mostrado en caso de error
- * \param lowLimit Limite inferior a validar
- * \param hiLimit Limite superior a validar
- * \return Si obtuvo el numero [0] si no [-1]
- *
- */
-void getIntt(int* input,char message[],char eMessage[], int lowLimit, int hiLimit);
-/**
- * \brief Solicita un número al usuario y lo valida
- * \param input Se carga el numero ingresado
- * \param message Es el mensaje a ser mostrado
- * \param eMessage Es el mensaje a ser mostrado en caso de error
- * \param lowLimit Limite inferior a validar
- * \param hiLimit Limite superior a validar
- * \return Si obtuvo el numero [0] si no [-1]
- *
- */
-int getFloatt(float* input,char message[],char eMessage[], float lowLimit, float hiLimit);
-/**
- * \brief Solicita un caracter al usuario y lo valida
- * \param input Se carga el caracter ingresado
- * \param message Es el mensaje a ser mostrado
- * \param eMessage Es el mensaje a ser mostrado en caso de error
- * \param lowLimit Limite inferior a validar
- * \param hiLimit Limite superior a validar
- * \return Si obtuvo el caracter [0] si no [-1]
- *
- */
-int getCharr(char* input,char message[],char eMessage[], char lowLimit, char hiLimit);
-/**
- * \brief Solicita una cadena de caracteres al usuario y la valida
- * \param input Se carga el string ingresado
- * \param message Es el mensaje a ser mostrado
- * \param eMessage Es el mensaje a ser mostrado en caso de error
- * \param lowLimit Longitud mínima de la cadena
- * \param hiLimit Longitud máxima de la cadena
- * \return Si obtuvo la cadena [0] si no [-1]
- *
- */
-int getStringg(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
-
-/**
- * \brief Solicita un número al usuario y devuelve el resultado
- * \param mensaje Es el mensaje a ser mostrado
- * \return El número ingresado por el usuario
- *
- */
-int getInt(char mensaje[]);
-/**
- * \brief Solicita un número al usuario y devuelve el resultado
- * \param mensaje Es el mensaje a ser mostrado
- * \return El número ingresado por el usuario
- *
- */
- float getFloat(char mensaje[]);
-/**
- * \brief Solicita un caracter al usuario y devuelve el resultado
- * \param mensaje Es el mensaje a ser mostrado
- * \return El número ingresado por el usuario
- *
- */
- char getChar(char mensaje[]);
-
-char getNumeroAleatorio(int desde , int hasta, int iniciar);
-
-int esNumerico(char str[]) ;
-
-void getString(char mensaje[],char input[]);
-int getInt3(char mensaje[],int* input,int lowLimit,int hiLimit);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void getString(char message[],char input[]);
+int getInt(char message[]);
+float getFloat(char message[]);
+char getChar(char message[]);
+char getNumeroAleatorio(int desde, int hasta, int iniciar);
+int esNumerico(char str[]);
+int esNumericoFlotante(char str[]);
+int esSoloLetras(char str[]);
+int getIntValid(int* input,char message[],char eMessage[],int lowLimit,int hiLimit);
+int getFloatValid(float* input,char message[],char eMessage[], float lowLimit, float hiLimit);
+int getCharValid(char* input,char message[],char eMessage[], char lowLimit, char hiLimit);
+int getStringLetras(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+int getStringNumeros(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+int getStringFlotantes(char* input,char message[],char eMessage[], int lowLimit, int hiLimit);
+int confirm();
 
 
 
--- funciones/main.c
+++ funciones/main.c
@@ -6,23 +6,33 @@ int main()
     int numero2=2;
     int numero;
     char letra;
+
+    int r;
     char respuesta;   //getIntt(&numero,"ingrese numero","error",1,10);
-    while(!(getCharr(&letra,"Ingrese letra[A][B][C]: ","Error ",'a','c')));
+
+
+    while((getCharValid(&letra,"Ingrese letra[A][B][C]: ","Error ",'a','c'))==-1);
     printf("%c\n",letra);
 
-    while(!(getStringNumeros("Ingrese numero entre 10 y 50: ",&numero,10,50)));
-    printf("%d",numero);
-
-    /*
-    numero=getInt2("Ingrese numero",1,7);
-    if(numero!=numero2)
-    {
-        printf("Error");
+    r=confirm();
+    if(r=0){
+    printf("Ingreso n");
     }
 
-    //respuesta=getCharr(&letra,"Ingrese letra Entre a y b","Error",'a','b');
-    //printf("La letra ingresada es %c",letra);
+/*
+    while(!(getStringNumeros("Ingrese numero entre 10 y 50: ",&numero,10,50)));
+    printf("%d",numero);
 */
+
 
         return 0;
 }
+
+
+
+
+
+
+
+
+
